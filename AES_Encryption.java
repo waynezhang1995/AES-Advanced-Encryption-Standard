07a6e251371546fc615f2471bb739a710dd58cc4
Wayne Zhang <waynezhangyuwei@Gmail.com>
2016-10-10 22:53:05 -0700

    update

--- AES_Encryption.java
+++ AES_Encryption.java
@@ -8,7 +8,7 @@ public class AES_Encryption {
 	private char[] sbox;
 	private byte[] key;
 	private byte[][] state;
-	private ArrayList<byte[]> w;
+	private byte[][] w;
 	private ArrayList<byte[]> source;
 
 	private final char[][] Rcon = { { 0x00, 0x00, 0x00, 0x00 }, { 0x01, 0x00, 0x00, 0x00 }, { 0x02, 0x00, 0x00, 0x00 },
@@ -20,19 +20,19 @@ public class AES_Encryption {
 		this.key = key;
 		this.source = source;
 		this.sbox = sbox;
-		this.w = new ArrayList<byte[]>();
 		this.state = new byte[4][4];
 	}
 
 	public void encrypt() {
 		int Nb = 4;
 		int Nr = 14;
-		
+
 		this.keyExpansion(this.key);
 		for (int i = 0; i < source.size(); i++) {
 			byte[] curr = source.get(i);
 			this.GenerateState(curr);
-			this.addRoundKey(this.state, this.source, 0, Nb );
+			this.addRoundKey(this.state, this.source, 0, Nb);
+
 		}
 	}
 
@@ -49,17 +49,21 @@ public class AES_Encryption {
 		int Nk = 8; // key length (in words)
 		int Nr = 14; // 14 round
 		// initialization
+		int totalWords = Nb * (Nr + 1);
+		this.w = new byte[totalWords][Nb];
 		for (int i = 0; i < Nk; i++) {
-			byte[] tmp = { this.key[4 * i], this.key[4 * i + 1], this.key[4 * i + 2], this.key[4 * i + 3] };
-			w.add(i, tmp);
+			for (int j = 0; j < 4; j++) {
+				this.w[i][j] = this.key[4 * i + j];
+			}
 		}
 
-		int totalWords = Nb * (Nr + 1);
 		// remaining words. we start from Nk
 		for (int i = Nk; i < totalWords; i++) {
-			byte[] tmp = new byte[Nb];
-			w.add(i, new byte[4]);
-			tmp = w.get(i - 1);
+
+			byte[] tmp = new byte[4];
+			for (int k = 0; k < 4; k++) {
+				tmp[k] = w[i - 1][k];
+			}
 			if (i % Nk == 0) {
 				tmp = this.SubWord(this.rotWord(tmp));
 				for (int k = 0; k < 4; k++) {
@@ -70,17 +74,14 @@ public class AES_Encryption {
 			}
 			byte[] result = new byte[4];
 			for (int j = 0; j < 4; j++) {
-				result[j] = (byte) (w.get(i - Nk)[j] ^ tmp[j]);
+				result[j] = (byte) (w[i - Nk][j] ^ tmp[j]);
 			}
-			w.add(i, result);
+			w[i] = result;
 		}
-
 	}
 
 	private byte[] SubWord(byte[] byteArray) {
 		for (int i = 0; i < 4; i++) {
-			// System.out.println("value is: " + (byteArray[i]&0xFF) + " Table
-			// has: "+(byte)this.sbox[byteArray[i]&0xFF]);
 			byteArray[i] = (byte) this.sbox[byteArray[i] & 0xFF];
 		}
 		return byteArray;
@@ -97,10 +98,10 @@ public class AES_Encryption {
 		return byteArray;
 	}
 
-	private void addRoundKey(byte [][] state, ArrayList<byte[]> w, int Round, int Nb) {
-		for(int i = 0 ; i <Nb ; i++){
-			for(int j = 0 ; j <Nb;j++){
-				state[i][j] ^= w.get(Round*Nb+j)[i];
+	private void addRoundKey(byte[][] state, ArrayList<byte[]> w, int Round, int Nb) {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < Nb; j++) {
+				state[i][j] ^= w.get(Round * Nb + j)[i];
 			}
 		}
 	}
